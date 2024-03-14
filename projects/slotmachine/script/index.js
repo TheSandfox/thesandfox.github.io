689193dbf8e6db2379c2aeaa7f5d63fb6dc735d7
thesandfox <sandfox9411@gmail.com>
2024-03-14 17:29:53 +0900

    240314 텍스트에리어 만들어야함

--- projects/slotmachine/script/index.js
+++ projects/slotmachine/script/index.js
@@ -1,32 +1,54 @@
 window.addEventListener('contextmenu',(event)=>{event.preventDefault();});
 
+let defaultItems = [
+	[
+		"심각한",
+		"유쾌한",
+		"경박한",
+		"진지한",
+		"신중한",
+		"화려한",
+		"신속한",
+		"강력한",
+		"냉혹한"
+	],
+	[
+		"치유술사",
+		"아이돌",
+		"너구리",
+		"엔지니어",
+		"팝가수",
+		"가설병",
+		"알파카",
+		"악어새",
+		"연금술사",
+		"연예인",
+		"패셔니스타"
+	],
+	[
+		"찰스",
+		"앨빈",
+		"데이브",
+		"크리스",
+		"앤드류",
+		"스즈키",
+		"슐레이만",
+		"앗딘",
+		"블랑카",
+		"유고",
+		"민수",
+		"네이만"
+	]
+];
+
+// localStorage.setItem(Slotmachine.LOCAL_ITEM_PREFIX+'0','aaa,bbb,ccc');
+
 let slotmachine = new Slotmachine('.slotmachine.first','랜덤 작명기');
 slotmachine.addDisplay();
 slotmachine.addDisplay();
 slotmachine.addDisplay();
-slotmachine.addDisplayItem(0,"심각한");
-slotmachine.addDisplayItem(0,"유쾌한");
-slotmachine.addDisplayItem(0,"경박한");
-slotmachine.addDisplayItem(0,"진지한");
-slotmachine.addDisplayItem(0,"신중한");
-slotmachine.addDisplayItem(0,"화려한");
-slotmachine.addDisplayItem(0,"신속한");
-slotmachine.addDisplayItem(0,"강력한");
-slotmachine.addDisplayItem(0,"냉혹한");
-slotmachine.addDisplayItem(1,"치유술사");
-slotmachine.addDisplayItem(1,"아이돌");
-slotmachine.addDisplayItem(1,"너구리");
-slotmachine.addDisplayItem(1,"엔지니어");
-slotmachine.addDisplayItem(1,"팝가수");
-slotmachine.addDisplayItem(1,"가설병");
-slotmachine.addDisplayItem(2,"찰스");
-slotmachine.addDisplayItem(2,"앨빈");
-slotmachine.addDisplayItem(2,"데이브");
-slotmachine.addDisplayItem(2,"크리스");
-slotmachine.addDisplayItem(2,"앤드류");
-slotmachine.addDisplayItem(2,"스즈키");
-slotmachine.addDisplayItem(2,"슐레이만");
-slotmachine.addDisplayItem(2,"앗딘");
-slotmachine.addDisplayItem(2,"블랑카");
-slotmachine.addDisplayItem(2,"유리");
+for(let i=0;i<slotmachine.displays.length;i++){
+	let localItem = localStorage.getItem(Slotmachine.LOCAL_ITEM_PREFIX+String(i));
+	slotmachine.importItems(i,localItem?localItem.split(','):defaultItems[i]);
+}
 slotmachine.setRandomProgress();
--- projects/slotmachine/script/slotmachine.js
+++ projects/slotmachine/script/slotmachine.js
@@ -30,14 +30,17 @@ class SlotmachineDisplay {
 	static mouseUpCallback() {
 		SlotmachineDisplay.CURRENT_SELECTED = null;
 	}
-	constructor() {
+	constructor(slotmachineInstance,defaultContent) {
+		this.machine = slotmachineInstance;
 		this.container = document.createElement('div');
+		this.container.classList.add('slotmachine-display-wrapper');
 		this.container.classList.add('dotbox');
 		this.display = document.createElement('div');
 		this.display.classList.add('slotmachine-display');
 		this.slot_container = document.createElement('div');
 		this.slot_container.classList.add('slot-container');
 		this.slots = this.slot_container.children;
+		this.hidden = false;
 		//append
 		this.container.appendChild(this.display);
 		this.display.appendChild(this.slot_container);
@@ -52,13 +55,19 @@ class SlotmachineDisplay {
 		this.timer = setInterval(()=>{
 			this.timerAction();
 		},25);
-		this.setSleep(true);
+		// this.setSleep(true);
+		this.sleep = true;
+		//import
+		if(defaultContent!==undefined){
+			console.log("얍")
+			this.importItems(defaultContent);
+		}
 		//push
 		SlotmachineDisplay.THIS.push(this);
 		SlotmachineDisplay.DISPLAYS.push(this.display);
 	}
 	timerAction() {
-		if(this.sleep){return;}
+		if(this.sleep||this.hidden){return;}
 		if(this==SlotmachineDisplay.CURRENT_SELECTED) {
 			this.progress += (this.autoProgress)*SlotmachineDisplay.TIMER_TICK;
 		} else {
@@ -104,18 +113,19 @@ class SlotmachineDisplay {
 			})
 		}
 		this.slot_container.appendChild(newSlot);
+		this.setRandomProgress();
 	}
 	clearItem() {
 		while (this.slot_container.firstChild) {
 			this.slot_container.removeChild(myNode.lastChild);
 		}
 	}
-	importItem(stringArr) {
+	importItems(stringArr) {
 		stringArr.forEach((val)=>{
 			this.addItem(val);
 		});
 	}
-	carculatePosition() {
+	clampProgress() {
 		//프로그레스 클램핑
 		if (this.slots.length<=1){return;}
 		if (this.progress<=-1.0) {
@@ -127,6 +137,23 @@ class SlotmachineDisplay {
 				this.progress -= this.slots.length;
 			}
 		}
+	}
+	getCurrentDisplayIndex() {
+		let val = this.progress;
+		if (this.slots.length<= 0){return 0;}
+		if (val < 0) {
+			while(val<0) {
+				val += this.slots.length;
+			}
+		} else if (val >= this.slots.length) {
+			while(val>=this.slots.length){
+				val -= this.slots.length;
+			}
+		}
+		return val;
+	}
+	carculatePosition() {
+		this.clampProgress();
 		for(let i = 0;i<this.slots.length;i++) {
 			//프로그레스 음수일 때 마지막 요소 위로
 			if (i==this.slots.length-1 && this.progress < 0.) {
@@ -142,9 +169,28 @@ class SlotmachineDisplay {
 			}
 		}
 	}
+	//
+	hide(flag) {
+		this.hidden = flag;
+		if (flag) {
+			this.suspend();
+			this.setRandomProgress();
+			this.container.classList.add('hidden');
+		} else {
+			this.container.classList.remove('hidden');
+			this.carculatePosition();
+		}
+	}
 	//휴면
 	setSleep(flag) {
+		if(this.slots.length<=1){return;}
 		this.sleep = flag;
+		if(flag){
+			this.clampProgress();
+			this.machine.sleepRequest();
+		} else {
+			this.machine.awake();
+		}
 	}
 	// 무작위 슬롯으로
 	setRandomProgress() {
@@ -158,6 +204,7 @@ class SlotmachineDisplay {
 	}
 	// 양수방향일 때 위로 올라감
 	moveWithGwansung(val) {
+		if (this.slots.length<=1){return;}
 		this.yVector.shift();
 		this.yVector.push(val);
 		this.gwansung = (this.yVector.reduce((a, b) => a + b, 0)*-10 / this.yVector.length);
@@ -168,6 +215,7 @@ class SlotmachineDisplay {
 	}
 	//굴리기
 	pull() {
+		if(this.slots.length<=1){return;}
 		let rval = Math.random();
 		let yval = (this.slots.length*0.5)+6+rval*2
 		this.machal = 1.5+rval*2;
@@ -179,15 +227,14 @@ class SlotmachineDisplay {
 
 class Slotmachine {
 	static THIS = [];
-	static BUTTONS = [];
 	static CURRENT_SELECTED_BUTTON = null;
-	static get(buttonElement){ 
-		return Slotmachine.THIS[Slotmachine.BUTTONS.indexOf(buttonElement)];
+	static LOCAL_ITEM_PREFIX = 'slotmachine-content';
+	static get(buttonElement){
+		return Slotmachine.THIS[buttonElement.dataset.index];
 	}
 	static releaseButton() {
 		if (Slotmachine.CURRENT_SELECTED_BUTTON != null) {
-			let bottom = Slotmachine.CURRENT_SELECTED_BUTTON.querySelector(' .bottom');
-			bottom.classList.remove('hidden');
+			Slotmachine.CURRENT_SELECTED_BUTTON.classList.remove('pressed');
 			Slotmachine.CURRENT_SELECTED_BUTTON = null;
 			return true;
 		} else {
@@ -200,8 +247,40 @@ class Slotmachine {
 		if(Slotmachine.CURRENT_SELECTED_BUTTON != null){
 			if (event.button==0&&event.target==Slotmachine.CURRENT_SELECTED_BUTTON) {
 				let slotmachineInstance = Slotmachine.get(Slotmachine.CURRENT_SELECTED_BUTTON);
-				slotmachineInstance.setRandomProgress();
-				slotmachineInstance.pull();
+				// 눌린 버튼 종류에 따라 분기
+				switch (Slotmachine.CURRENT_SELECTED_BUTTON.dataset.buttonName) {
+					case 'pull' :
+						//슬롯돌리기
+						slotmachineInstance.setRandomProgress();
+						slotmachineInstance.pull();
+						break;
+					case 'quick' :
+						//즉시생성
+						slotmachineInstance.setRandomProgress();
+						slotmachineInstance.suspend();
+						slotmachineInstance.awake();
+						slotmachineInstance.sleepRequest();
+						break;
+					case 'plus' :
+						//슬롯늘리기
+						slotmachineInstance.setDisplayCount(slotmachineInstance.displayCount+1);
+						break;
+					case 'minus' :
+						//슬롯줄이기
+						slotmachineInstance.setDisplayCount(slotmachineInstance.displayCount-1);
+						break;
+					case 'copy' :
+						let content = slotmachineInstance.resultDisplay.children[0].innerText;
+						navigator.clipboard.writeText(content)
+							.then(() => {
+							// console.log("Text copied to clipboard...")
+						})
+							.catch(err => {
+							// console.log('Something went wrong', err);
+						})
+						break;
+					default :
+				}
 			}
 		}
 		Slotmachine.releaseButton()
@@ -209,9 +288,9 @@ class Slotmachine {
 	static mouseDownCallback(event) {
 		if(event.button!=0&&event.button!=2){return;}
 		if(event.button==0) {
+			event.preventDefault();
 			Slotmachine.CURRENT_SELECTED_BUTTON = event.target;
-			let bottom = Slotmachine.CURRENT_SELECTED_BUTTON.querySelector(' .bottom');
-			bottom.classList.add('hidden');
+			Slotmachine.CURRENT_SELECTED_BUTTON.classList.add('pressed');
 		} else {
 			Slotmachine.releaseButton();
 		}
@@ -219,17 +298,20 @@ class Slotmachine {
 	constructor(selector,name) {
 		this.container = document.querySelector(selector);
 		this.displays = [];
-		this.button = null;
+		this.buttons = [];
+		this.index = Slotmachine.THIS.push(this) - 1;
 		this.initTitle(name);
 		this.initDisplayContainer();
-		this.initButton();
-		Slotmachine.THIS.push(this);
-		Slotmachine.BUTTONS.push(this.button);
+		this.initButtons();
+		this.initResultDisplay();
+		this.displayCount = 0;
+		return this;
 	}
-	addDisplay() {
-		let display = new SlotmachineDisplay();
+	addDisplay(defaultContent) {
+		let display = new SlotmachineDisplay(this,defaultContent);
 		this.display_container.appendChild(display.container);
 		this.displays.push(display);
+		this.displayCount += 1;
 	}
 	addDisplayItem(ind,val) {
 		this.displays[ind].addItem(val);
@@ -241,7 +323,9 @@ class Slotmachine {
 	}
 	setRandomProgress() {
 		for(let i = 0;i<this.displays.length;i++) {
-			this.displays[i].setRandomProgress();
+			if(!this.displays[i].hidden){
+				this.displays[i].setRandomProgress();
+			}
 		}
 	}
 	initTitle(name) {
@@ -257,30 +341,111 @@ class Slotmachine {
 		this.display_container.classList.add('slotmachine-display-container');
 		this.container.appendChild(this.display_container);
 	}
-	initButton() {
-		this.button = document.createElement('div');
-		this.button.classList.add('slotmachine-button');
-		let dbox = document.createElement('div');
-		dbox.classList.add('dotbox');
-		this.button.appendChild(dbox);
-		let plastic = document.createElement('div');
-		plastic.classList.add('plastic');
-		let top = document.createElement('div');
-		top.classList.add('top');
-		let bottom = document.createElement('div');
-		bottom.classList.add('bottom');
-		top.innerHTML = 'GO!';
+	initButtons() {
+		let buttonContainer = document.createElement('div');
+		buttonContainer.classList.add('slotmachine-button-container');
+		for(let i = 0;i<4;i++){
+			this.buttons[i] = document.createElement('div');
+			this.buttons[i].classList.add('slotmachine-button');
+			let dbox = document.createElement('div');
+			dbox.classList.add('dotbox');
+			this.buttons[i].appendChild(dbox);
+			let plastic = document.createElement('div');
+			plastic.classList.add('plastic');
+			let top = document.createElement('div');
+			top.classList.add('top');
+			let bottom = document.createElement('div');
+			bottom.classList.add('bottom');
+			this.buttons[i].dataset.index=this.index;
+			// append
+			buttonContainer.appendChild(this.buttons[i]);
+			this.buttons[i].appendChild(dbox);
+			this.buttons[i].appendChild(plastic);
+			plastic.appendChild(top);
+			plastic.appendChild(bottom);
+			// addevent
+			this.buttons[i].addEventListener('mousedown',(event)=>{Slotmachine.mouseDownCallback(event);});
+		}
+		// indivisual content
+		this.buttons[0].querySelector(' .top').innerHTML = 'GO!';
+		this.buttons[1].querySelector(' .top').innerHTML = '⚡';
+		this.buttons[2].querySelector(' .top').innerHTML = '+';
+		this.buttons[3].querySelector(' .top').innerHTML = '-';
+		this.buttons[1].classList.add('quick');
+		this.buttons[2].classList.add('plus');
+		this.buttons[3].classList.add('minus');
+		this.buttons[0].dataset.buttonName='pull';
+		this.buttons[1].dataset.buttonName='quick';
+		this.buttons[2].dataset.buttonName='plus';
+		this.buttons[3].dataset.buttonName='minus';
 		// append
-		this.container.appendChild(this.button);
-		this.button.appendChild(dbox);
-		this.button.appendChild(plastic);
-		plastic.appendChild(top);
-		plastic.appendChild(bottom);
-		// addevent
-		this.button.addEventListener('mousedown',(event)=>{Slotmachine.mouseDownCallback(event);});
+		this.container.appendChild(buttonContainer);
+	}
+	initResultDisplay() {
+		this.resultDisplay = document.createElement('div');
+		this.resultDisplay.classList.add('slotmachine-result-display');
+		let resultText = document.createElement('div');
+		resultText.classList.add('text');
+		let copyButton = document.createElement('i');
+		copyButton.className = "fi fi-bs-copy-alt copy";
+		//append
+		this.container.appendChild(this.resultDisplay);
+		this.resultDisplay.appendChild(resultText);
+		this.resultDisplay.appendChild(copyButton);
+		//add event
+		copyButton.dataset.index=this.index;
+		copyButton.dataset.buttonName='copy';
+		copyButton.addEventListener('mousedown',(event)=>{Slotmachine.mouseDownCallback(event);});
+	}
+	suspend() {
+		for(let i = 0; i<this.displays.length; i++) {
+			this.displays[i].suspend();
+		}
+	}
+	sleepRequest() {
+		let s = '';
+		for(let i = 0; i<this.displays.length; i++) {
+			if ((!this.displays[i].sleep)&&(!this.displays[i].hidden)){return;}
+		}
+		for(let i = 0; i<this.displays.length; i++) {
+			if (this.displays[i].hidden||this.displays[i].slots.length<=0) {continue;}
+			if (i==0) {
+				s+= this.displays[i].slots[this.displays[i].getCurrentDisplayIndex()].innerHTML
+			} else {
+				s+=' '+this.displays[i].slots[this.displays[i].getCurrentDisplayIndex()].innerHTML;
+			}
+		}
+		this.resultDisplay.children[0].innerText = s;
+		this.resultDisplay.classList.add('active');
+	}
+	awake() {
+		this.resultDisplay.classList.remove('active');
+	}
+	setDisplayCount(val) {
+		let c = val;
+		if(c<=0) {return;};
+		if (c>this.displays.length) {
+			for(let i=this.displays.length;i<c;i++) {
+				this.addDisplay(["-"]);
+			}
+		}
+		this.displayCount = c;
+		for(let i = 0;i<this.displays.length;i++) {
+			if (i<c) {
+				this.displays[i].hide(false);
+			} else {
+				this.displays[i].hide(true);
+			}
+		}
+		this.sleepRequest();
+	}
+	importItems(index,val) {
+		if(index<0||index>=this.displays.length){return;}
+		this.displays[index].importItems(val);
 	}
 }
 
+// essencial configuration
 window.addEventListener('mousemove',SlotmachineDisplay.moveCallback);
 window.addEventListener('mouseup',SlotmachineDisplay.mouseUpCallback);
 window.addEventListener('mouseup',(event)=>{Slotmachine.mouseUpCallback(event);});
